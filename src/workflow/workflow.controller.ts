f1f28ee6079e38510acb22ef5333232c0c63a7b0
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2026-02-26 12:15:26 +0100

    feat: add generic controller filter

--- src/workflow/workflow.controller.ts
+++ src/workflow/workflow.controller.ts
@@ -1,12 +1,14 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Post, UseFilters} from '@nestjs/common';
 import { WorkflowService } from './workflow.service';
+import {AllExceptionsFilter} from "./workflow.filter";
 
 @Controller('/v1')
+@UseFilters(AllExceptionsFilter)
 export class WorkflowController {
   constructor(private readonly appService: WorkflowService) {}
 
   @Post('/workflows/trigger')
   async triggerWorkflow(@Body() payload: any): Promise<void> {
-    this.appService.startWorkflow();
+    this.appService.startWorkflow(payload);
   }
 }
