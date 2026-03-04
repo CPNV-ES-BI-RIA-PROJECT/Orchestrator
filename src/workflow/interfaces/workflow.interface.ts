ec6cb44d1b1ed9b88fa9fe939cbcbe9c13d29e43
walvie <walvieswisel@gmail.com>
2026-03-04 22:24:50 +0100

    feat: added return type for workflow interface

--- src/workflow/interfaces/workflow.interface.ts
+++ src/workflow/interfaces/workflow.interface.ts
@@ -1,5 +1,11 @@
 import { WorkflowContext } from '../models/workflow-context.model';
 
+export interface WorkflowResult<T = void> {
+  isSuccess: boolean;
+  data?: T;
+  error?: unknown;
+}
+
 export interface IWorkflow<TPayload = unknown, TResult = void> {
-  execute(context: WorkflowContext<TPayload>): Promise<TResult>;
+  execute(context: WorkflowContext<TPayload>): Promise<WorkflowResult<TResult>>;
 }
