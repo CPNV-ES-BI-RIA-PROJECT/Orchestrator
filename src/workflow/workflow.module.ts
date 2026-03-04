edffb8c838cf10749aa733042a175e5b9609e9cc
walvie <walvieswisel@gmail.com>
2026-03-05 00:25:58 +0100

    fix: update workflow module file

--- src/workflow/workflow.module.ts
+++ src/workflow/workflow.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { WorkflowController } from './workflow.controller';
 import { WorkflowService } from './workflow.service';
 import { ClientModule } from '../client/client.module';
+import { ETLWorkflow } from './strategies/etl-workflow.service';
+import { ExtractWorkflowStepService } from './strategies/steps/extract-workflow-step.service';
+import { STEPS_TOKEN } from './workflow.constants';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ClientModule],
+  imports: [ClientModule, ConfigModule.forRoot()],
   controllers: [WorkflowController],
-  providers: [WorkflowService],
+  providers: [
+    WorkflowService,
+    ETLWorkflow,
+    ExtractWorkflowStepService,
+    {
+      provide: STEPS_TOKEN,
+      useFactory: (extract: ExtractWorkflowStepService) => {
+        return [extract];
+      },
+      inject: [ExtractWorkflowStepService],
+    },
+  ],
 })
 export class WorkflowModule {}
