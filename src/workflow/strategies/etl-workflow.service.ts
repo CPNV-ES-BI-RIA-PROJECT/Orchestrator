b53b308fb33076411e457b92d51de05f88bab027
walvie <walvieswisel@gmail.com>
2026-03-04 22:25:10 +0100

    feat: implemented etl workflow service

---
+++ src/workflow/strategies/etl-workflow.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { IWorkflow, WorkflowResult } from '../interfaces/workflow.interface';
+import { WorkflowContext } from '../models/workflow-context.model';
+import { IWorkflowStep } from '../interfaces/workflow-step.interface';
+
+@Injectable()
+export class ETLWorkflow<TPayload, TResults> implements IWorkflow<
+  TPayload,
+  TResults
+> {
+  constructor(
+    @Inject('STEPS_TOKEN')
+    private readonly steps: IWorkflowStep<TPayload>[],
+  ) {}
+
+  async execute(
+    context: WorkflowContext<TPayload>,
+  ): Promise<WorkflowResult<TResults>> {
+    for (const [, step] of this.steps.entries()) {
+      try {
+        const result = await step.execute(context);
+
+        if (!result.isSuccess) {
+          return { isSuccess: false, error: result.error };
+        }
+      } catch (error) {
+        return { isSuccess: false, error: error };
+      }
+    }
+
+    return { isSuccess: true };
+  }
+}
