861323b7eabbf63a381a1b6b70834409b8cee9a5
walvie <walvieswisel@gmail.com>
2026-03-04 23:23:26 +0100

    feat: implement http client service

--- src/client/http/http-client.service.ts
+++ src/client/http/http-client.service.ts
@@ -1,1 +1,20 @@
-export class HttpClientService {}
+import { IClient } from '../interfaces/client.interface';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class HttpClientService implements IClient {
+  constructor(private httpService: HttpService) {}
+
+  async post<TPayload, TResult>(
+    target: string,
+    payload: TPayload,
+  ): Promise<TResult> {
+    const response = await firstValueFrom(
+      this.httpService.post<TResult>(target, payload),
+    );
+
+    return response.data;
+  }
+}
