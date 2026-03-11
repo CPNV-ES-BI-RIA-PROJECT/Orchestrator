584f3c2842cb987102f85bedb885e2ebd3b625ea
walvie <walvieswisel@gmail.com>
2026-03-11 21:19:32 +0100

    feat: create client configuration

---
+++ src/client/config/client.config.ts
@@ -0,0 +1,11 @@
+import { registerAs } from '@nestjs/config';
+import { ClientConfig } from '../interfaces/client-config.interface';
+
+export default registerAs(
+  'client',
+  (): ClientConfig => ({
+    type: process.env.CLIENT_TYPE || '',
+    timeout: parseInt(process.env.CLIENT_TIMEOUT || '5000', 10),
+    baseUrl: process.env.CLIENT_BASE_URL || '',
+  }),
+);
