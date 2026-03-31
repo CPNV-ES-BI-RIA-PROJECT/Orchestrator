c25e693d1eb6238295f60b9fb901122892478fca
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2026-03-31 11:38:44 +0200

    feat: check cache before running workflow

--- src/workflow/workflow.service.ts
+++ src/workflow/workflow.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { ConflictException, Inject, Injectable, Logger } from '@nestjs/common';
 import { ETLWorkflow } from './strategies/etl-workflow.service';
 import { WorkflowContext } from './models/workflow-context.model';
 import { WorkflowResult } from './interfaces/workflow.interface';
+import { CacheService } from '../cache/cache.service';
 
 export interface Payload {
   url: string;
@@ -12,10 +13,13 @@ export class WorkflowService {
   private readonly logger = new Logger(WorkflowService.name);
 
   constructor(
+    @Inject(CacheService) private readonly cacheService: CacheService,
     private readonly etlWorkflow: ETLWorkflow<Payload, WorkflowResult>,
   ) {}
 
   async startWorkflow(url: string): Promise<void> {
+    await this.checkCache(url);
+
     const context = new WorkflowContext<Payload>('1', { url });
     this.logger.log(`Triggering workflow for job ${context.jobId}`);
 
@@ -27,5 +31,16 @@ export class WorkflowService {
     }
 
     this.logger.log(`Workflow finished for job ${context.jobId}`);
+  }
+
+  private async checkCache(url: string) {
+    const cacheResult = await this.cacheService.check({
+      urlJson: { url: url },
+    });
+
+    if (cacheResult.alreadyProcessed) {
+      this.logger.log(`This request has already been processed.`);
+      throw new ConflictException('Request has already been processed.');
+    }
   }
 }
