9595a8cdc624c1c7d8982b1c617ae9c3fdf03d75
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2026-03-31 10:27:07 +0200

    feat: make cache key serviec injectable

--- src/cache/cache-key.service.ts
+++ src/cache/cache-key.service.ts
@@ -1,12 +1,14 @@
 import hash from 'object-hash';
+import { Injectable } from '@nestjs/common';
 
 export interface CacheBusinessRequest {
   urlJson: Record<string, string>;
   paramsJson?: Record<string, unknown>;
 }
 
+@Injectable()
 export class CacheKeyService {
-  buildCacheKey(request: CacheBusinessRequest) {
+  buildCacheKey(request: CacheBusinessRequest): string {
     return hash(request);
   }
 }
